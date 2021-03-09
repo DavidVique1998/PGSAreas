eb3966d7a4a1d97ca745550db2b377ee1394b4f7
David Vique <davique@espe.edu.ec>
2021-03-09 11:02:04 -0500

    get area triangle

--- area.java
+++ area.java
@@ -4,16 +4,14 @@ import java.util.*;
 public class HelloWorld{
 
      public static void main(String []args){
-
-        
         Scanner scanner = new Scanner(System.in);
         System.out.println("Calcular el área");
-        System.out.print("Ingrese el lado: ");
-        float a = scanner.nextFloat(); 
-        System.out.print("Ingrese las unidades: ");
-        scanner.nextLine(); 
+        System.out.print("Ingrese la base");
+        float b= scanner.nextFloat(); 
+        System.out.print("Ingrese la altura");
+        float a= scanner.nextFloat(); 
         String unids = scanner.nextLine(); 
-        float result=a*a;
+        float result=(b*a)/2;
         System.out.print("resultado:"+result);
      }
 }
